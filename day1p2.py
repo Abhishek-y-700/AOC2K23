38648b5ee1b9d8fb2decc53ed86b94e0f4da0454
Abby <113952698+Abhishek-y-700@users.noreply.github.com>
2023-12-03 18:52:40 -0400

    Recog digit and Var

--- day1p2.py
+++ day1p2.py
@@ -5,7 +5,7 @@ with open('inputd1p1.txt','r')as file:
 
 
 # Eg val inp
-example= ["two1nine","eightwothree","three33"]
+example= ["two1nine","eightwothree"]
 def day1func(example):
     sum=0
     var_digit={ "one":1 , "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8,"nine":9 }
@@ -15,8 +15,9 @@ def day1func(example):
         for y in var_digit:           
           if y in x:
                print(" There exists a var digit")
-        """ if any(char.isdigit() for char in x):  #To check presence of int digit
-             print(" There exists an int digit") """
+        for char in x:
+                if char.isdigit():  #To check presence of int digit
+                        print(" There exists an int digit")
         print("End func")
     
     
---
+++ spam.py
@@ -0,0 +1,4 @@
+x="three3443"
+for char in x:
+    if (char.isdigit()):  #To check presence of int digit
+                print(" There exists an int digit")

cc79ffe6e78ffbbe12c837abaeb111a84b7cb194
Abby <113952698+Abhishek-y-700@users.noreply.github.com>
2023-12-04 02:21:02 -0400

    Working d1p2 on example

--- day1p2.py
+++ day1p2.py
@@ -1,27 +1,29 @@
-# Day 1  AOC 2k23
+# Day 1 p2  AOC 2k23
+import re
 with open('inputd1p1.txt','r')as file:
         file_content=file.read()
         inputval=(file_content).splitlines() #splits to list
-
-
+#print(inputval)
+var_digit = { "one":1 , "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8,"nine":9 }
 # Eg val inp
-example= ["two1nine","eightwothree"]
+example= ['sdpgz3five4seven6fiveh', '876mbxbrntsfm', 'fivek5mfzrdxfbn66nine8eight']
 def day1func(example):
     sum=0
-    var_digit={ "one":1 , "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8,"nine":9 }
-    for x in example: 
-        # Return first element two1nine= ["2","1","9"], then add index first and last
-        #numlist = list(''.join(filter(str.isdigit, x)))
-        for y in var_digit:           
-          if y in x:
-               print(" There exists a var digit")
-        for char in x:
-                if char.isdigit():  #To check presence of int digit
-                        print(" There exists an int digit")
-        print("End func")
-    
+    for char in example:
+        
+        numlist = re.findall(r'(\d|one|two|three|four|five|six|seven|eight|nine)', char)
+        print(numlist)
+        print([var_digit.get(i, i) for i in numlist])
+        tuple=[var_digit.get(i, i) for i in numlist]
+        
+        sum_tuple= int(tuple[0])*10 +int(tuple[len(tuple)-1])
+        print(sum_tuple)
+        sum= sum+ sum_tuple
+        print(sum  ) 
+        print('End func')
+    return sum
     
 
-print(day1func(example))
+print(day1func(inputval))
             
         
--- spam.py
+++ spam.py
@@ -1,4 +1,18 @@
-x="three3443"
-for char in x:
-    if (char.isdigit()):  #To check presence of int digit
-                print(" There exists an int digit")
+import re
+
+var_digit = { "one":1 , "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8,"nine":9 }
+example = ["two1nine","eightwothree","abcone2threexyz","xtwone3four","4nineeightseven2","zoneight234","7pqrstsixteen"]
+sum=0
+for char in example:
+    
+    numlist = re.findall(r'(\d|one|two|three|four|five|six|seven|eight|nine)', char)
+    print(numlist)
+    print([var_digit.get(i, i) for i in numlist])
+    tuple=[var_digit.get(i, i) for i in numlist]
+    
+    sum_tuple= int(tuple[0])*10 +int(tuple[len(tuple)-1])
+    print(sum_tuple)
+    sum= sum+ sum_tuple
+    print(sum  ) 
+    print('End func')
+

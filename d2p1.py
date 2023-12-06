06da903d4244c4aa7c5bf12c53c094322cf95dcc
Abby <113952698+Abhishek-y-700@users.noreply.github.com>
2023-12-05 20:08:54 -0400

    extracting all nums

--- d2p1.py
+++ d2p1.py
@@ -6,7 +6,7 @@ with open('d2ip.txt','r')as file:
     inputval=(file_content).splitlines() #splits to list
 
 # possibility = 12 red cubes, 13 green cubes, and 14 blue cubes
-# '\b([1-9]|1[0-2])\b\sred'gi to find red 12 | red= re.findall(r'(`\b([1-9]|1[0-2])\b\sred`ig)',game)
+# '\b([1-9]|1[0-2])\b\sred'gi to find red 12 |  ([1][0-2]\sred) | red_pat=r'([1][0-2]|[1-9])\sred'
 def cubes(inp):
     red= 12
     green=13
@@ -14,8 +14,24 @@ def cubes(inp):
     game_no=0
     for game in inp:
         game_no+=1
-        red= re.findall(r'([1-9]|1[0-2])\sred',game)
-        print(red)
+
+        red_pat=r'(\d{2}|\d)\sred'
+        green_pat=r'(\d{2}|\d)\sgreen'
+        blue_pat=r'(\d{2}|\d)\sblue'
+        red_balls= re.findall(red_pat,game)
+        green_balls= re.findall(red_pat,game)
+        blue_balls= re.findall(blue_pat,game)
+        limit_func(12,blue_balls)
+        limit_func(12,blue_balls)
+        limit_func(12,blue_balls)
+
+        
+def limit_func(limit,set):
+    for x in set:
+        if set>limit:
+            return False
+        else:
+            return True
         
 
 cubes(inputval)
--- spam.py
+++ spam.py
@@ -1,44 +1,35 @@
+# AOC 2k23 D2 P1
+#Cube Conundrum
 import re
-
-with open('inputd1p1.txt','r')as file:
+with open('d2ip.txt','r')as file:
     file_content=file.read()
     inputval=(file_content).splitlines() #splits to list
 
-var_digit = { "one":1 , "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8,"nine":9 }
-example = ["eightwothreeoneighttwone","onetwoneight"]
-
-def subs(inputtt):
-    list_new=[]
-    for x in inputtt:
-        char_rep= re.sub('one', 'o1e', x, re.I)
-        char_rep= re.sub('two', 't2o', char_rep, re.I)
-        char_rep= re.sub('three', 't3e', char_rep, re.I)
-        char_rep= re.sub('four', 'f4r', char_rep, re.I)
-        char_rep= re.sub('five', 'f5e', char_rep, re.I)
-        char_rep= re.sub('six', 's6x', char_rep, re.I)
-        char_rep= re.sub('seven', 's7n', char_rep, re.I)
-        char_rep= re.sub('eight', 'e8t', char_rep, re.I)
-        char_rep= re.sub('nine', 'n9e', char_rep, re.I)
-        list_new.append(str(char_rep))
-
-    return list_new
-
-subs_calc=subs(inputval)
-
-def mainfunc(subs_calc):
+# possibility = 12 red cubes, 13 green cubes, and 14 blue cubes
+# '\b([1-9]|1[0-2])\b\sred'gi to find red 12 | red= re.findall(r'(`\b([1-9]|1[0-2])\b\sred`ig)',game)
+def cubes(inp):
+    red =["1 red"]
+    """ red= 12
+    green=13
+    blue=14 """
+    red_pat=r'(\d\d\sred)|(\d\sred)'
+    game_no=0
     sum=0
-    for char in subs(subs_calc):
-        numlist = re.findall('(?=(one|two|three|four|five|six|seven|eight|nine|\d))', char)
-
-        print(numlist)
-        print([var_digit.get(i, i) for i in numlist])
-        tuple=[var_digit.get(i, i) for i in numlist]
+    for game in inp:
+        red = []
         
-        sum_tuple= int(tuple[0])*10 +int(tuple[len(tuple)-1])
-        print(sum_tuple)
-        sum= sum+ sum_tuple
-        print(sum) 
-        print('End func')
-    return sum  # Moved outside of the for loop
+        game_no+=1
+        
+        red_pat=r'([0-1][0-2])\sred|\d\sred'
+        red=re.findall(red_pat,game)
+        print(red)
+        
+        if any(int for x in red):
+            print(" This set of values is Accepted")
+        else:
+            print(" This set has no acceptable value for red balls")
 
-print(mainfunc(subs_calc))
+        
+        
+
+cubes(inputval)

fb1c30903d11a8030e248421ed4b6b3461a87e02
Abby <113952698+Abhishek-y-700@users.noreply.github.com>
2023-12-13 18:42:39 -0400

    AOC 2k23 d2p1 soln

--- d2p1.py
+++ d2p1.py
@@ -10,31 +10,46 @@ with open('d2ip.txt','r')as file:
 def cubes(inp):
     
     game_no=0
+    sum=0
     for game in inp:
         game_no+=1
 
         red_pat=r'(\d{2}|\d)\sred'
         green_pat=r'(\d{2}|\d)\sgreen'
         blue_pat=r'(\d{2}|\d)\sblue'
+        
         red_balls= re.findall(red_pat,game)
         green_balls= re.findall(green_pat,game)
         blue_balls= re.findall(blue_pat,game)
+        
         red=limit_func(12,red_balls)
         green=limit_func(13,green_balls)
         blue=limit_func(14,blue_balls)
-        print(red)
+        """ print("For game set no:",game_no," Red:",red,"Green:",green,"Blue:",blue) """
+        if red==True & green==True & blue==True:
+            sum+=game_no
+            if game_no==100:
+                print(sum)      
+    
+
 
         
 
         
 def limit_func(limit,set):
+    """ print('set is:',set)
+    print('the limit is',limit) """
     for x in set:
         if int(x)>limit:
             
-            return False
+            flag= False
+            break
+            
+        
         else:
-            return True
-    
+            
+            flag= True
+    return flag
 
     
 
--- spam.py
+++ spam.py
@@ -6,30 +6,51 @@ with open('d2ip.txt','r')as file:
     inputval=(file_content).splitlines() #splits to list
 
 # possibility = 12 red cubes, 13 green cubes, and 14 blue cubes
-# '\b([1-9]|1[0-2])\b\sred'gi to find red 12 | red= re.findall(r'(`\b([1-9]|1[0-2])\b\sred`ig)',game)
+# '\b([1-9]|1[0-2])\b\sred'gi to find red 12 |  ([1][0-2]\sred) | red_pat=r'([1][0-2]|[1-9])\sred'
 def cubes(inp):
-    red =["1 red"]
-    """ red= 12
-    green=13
-    blue=14 """
-    red_pat=r'(\d\d\sred)|(\d\sred)'
+    
     game_no=0
     sum=0
     for game in inp:
-        red = []
-        
         game_no+=1
+
+        red_pat=r'(\d{2}|\d)\sred'
+        green_pat=r'(\d{2}|\d)\sgreen'
+        blue_pat=r'(\d{2}|\d)\sblue'
         
-        red_pat=r'([0-1][0-2])\sred|\d\sred'
-        red=re.findall(red_pat,game)
-        print(red)
+        red_balls= re.findall(red_pat,game)
+        green_balls= re.findall(green_pat,game)
+        blue_balls= re.findall(blue_pat,game)
         
-        if any(int for x in red):
-            print(" This set of values is Accepted")
-        else:
-            print(" This set has no acceptable value for red balls")
+        red=limit_func(12,red_balls)
+        green=limit_func(13,green_balls)
+        blue=limit_func(14,blue_balls)
+        """ print("For game set no:",game_no," Red:",red,"Green:",green,"Blue:",blue) """
+        if red==True & green==True & blue==True:
+            sum+=game_no
+            if game_no==100:
+                print(sum)      
+    
+
 
         
+
         
+def limit_func(limit,set):
+    """ print('set is:',set)
+    print('the limit is',limit) """
+    for x in set:
+        if int(x)>limit:
+            
+            flag= False
+            break
+            
+        
+        else:
+            
+            flag= True
+    return flag
+
+    
 
 cubes(inputval)
